191b25f29451f0ba13ac2fd03cf5aaef694befab
martonMeszaros <marton.m95@gmail.com>
2017-09-24 23:02:29 +0200

    Argument solution

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 using std::cout;
 using std::endl;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            cout << "Hello " << argv[i] << "!" << endl;
+        }
+    } else {
+        cout << "Hello, World!" << endl;
+    }
     return 0;
 }
