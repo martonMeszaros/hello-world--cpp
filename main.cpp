a2b714552e73e167e86b862b887707d3b9efd596
martonMeszaros <marton.m95@gmail.com>
2017-09-27 10:37:47 +0200

    White space friendly input

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using std::string;
 int main() {
     string name;
     cout << "Please enter your name!" << endl;
-    cin >> name;
+    getline(cin, name);
     cout << "Hello " << name << "!" << endl;
     return 0;
 }
